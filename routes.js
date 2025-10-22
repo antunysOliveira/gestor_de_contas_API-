ad345ba298d419290e5a690d8920d0733d5225f5
Antunys Pereira <apereira@monumenta.com.br>
2025-10-22 14:09:04 -0300

    comentando rota de recuperar senha

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const authController = require('./controllers/authController');
 
 router.post('/login', authController.Login);
 router.post('/register', authController.register);
-router.post('/recover-password', authController.recoverPassword);
+// router.post('/recover-password', authController.recoverPassword);
 
 
 module.exports = router;
